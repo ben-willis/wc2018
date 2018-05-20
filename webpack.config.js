d1145cf5c3809c4da97a0f975d7d7c0fd473cd08
Ben Willis <benwillis0612@gmail.com>
2018-05-20 16:36:21 +0100

    Initial wepack configuration

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+
+				options: {
+					presets: ['env']
+				}
+			},
+			{
+				test: /\.(scss|css)$/,
+
+				use: [
+					{
+						loader: 'style-loader'
+					},
+					{
+						loader: 'css-loader'
+					},
+					{
+						loader: 'sass-loader'
+					}
+				]
+			}
+		]
+	},
+
+	plugins: [new UglifyJSPlugin()],
+	entry: src.js,
+
+	output: {
+		filename: '[name].[chunkhash].js',
+		path: path.resolve(__dirname, '/')
+	},
+
+	mode: 'development'
+};
