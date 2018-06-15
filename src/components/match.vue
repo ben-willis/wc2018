f9c091227540424d1bb23f7b118412e0f474787d
Ben Willis <benwillis0612@gmail.com>
2018-06-15 21:24:37 +0100

    Refactor match vue

--- src/components/match.vue
+++ src/components/match.vue
@@ -2,9 +2,9 @@
   <div class="fixture">
     <div class="match">
       <div class="home"><router-link :to="'/team/'+home_team_id">{{home_team}}</router-link></div>
-      <div v-if="status>0" class="scores">
-        <span :class="{live: (status==1)}">{{home_goals}}</span>
-        <span :class="{live: (status==1)}">{{away_goals}}</span>
+      <div v-if="status!='TIMED'" class="scores">
+        <span :class="{live: (status=='IN_PLAY')}">{{home_goals}}</span>
+        <span :class="{live: (status=='IN_PLAY')}">{{away_goals}}</span>
       </div>
       <div v-else class="time">
         <span>{{ kickoff | time }}</span>
@@ -16,47 +16,23 @@
 
 <script>
 export default {
-  computed: {
-    kickoff () {
-      return new Date(this.match.date);
-    },
-    home_team () {
-      if (this.match.homeTeamName) {
-        return this.match.homeTeamName
-      } else if (this.match.matchday === 8) {
-        return "England"
-      } else {
-        return "TBD"
-      }
-    },
-    home_goals () {
-      return this.match.result.goalsHomeTeam;
-    },
-    away_team () {
-        return this.match.awayTeamName ? this.match.awayTeamName : "TBD";
-    },
-    away_goals () {
-      return this.match.result.goalsAwayTeam;
-    },
-    home_team_id () {
-      return this.match._links.homeTeam.href.split("/").pop();
-    },
-    away_team_id () {
-      return this.match._links.awayTeam.href.split("/").pop();
-    },
-    status () {
-      if (this.match.status === "FINISHED") {
-        return 2;
-      } else if (this.match.status === "IN_PLAY") {
-        return 1;
-      } else {
-        return 0;
-      }
-    },
-    update_interval () {
-      return setInterval(function() {
+  data () {
+    return {
+      kickoff: new Date(this.match.date),
+      home_team: (this.match.homeTeamName) ? this.match.homeTeamName : "TBD",
+      away_team: (this.match.awayTeamName) ? this.match.awayTeamName : "TBD",
+      home_goals: this.match.result.goalsHomeTeam,
+      away_goals: this.match.result.goalsAwayTeam,
+      home_team_id: this.match._links.homeTeam.href.split("/").pop(),
+      away_team_id: this.match._links.awayTeam.href.split("/").pop(),
+      status: this.match.status
+    }
+  },
+  created () {
+    if (this.match.status == "IN_PLAY") {
+      this.update_interval = setInterval(function() {
         this.update_score()
-      }.bind(this), 1 * 1000);
+      }.bind(this), 30*1000);
     }
   },
   methods: {
@@ -67,11 +43,8 @@ export default {
       }).then((response) => {
         this.home_goals = response.body.fixture.result.goalsHomeTeam;
         this.away_goals = response.body.fixture.result.goalsAwayTeam;
-        if (this.match.status === "FINISHED") {
+        if (response.body.fixture.status === "FINISHED") {
           clearInterval(this.updateInterval);
-          this.status = 2;
-        } else if (this.match.status === "IN_PLAY") {
-          this.status = 1;
         }
       });
     }
